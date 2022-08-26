90c2dd5b81de2e2946eb0dd6d7e81198247f99db
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-08-26 10:12:22 -0300

    criando arquivos de rotas, controller e views

---
+++ src/routers/router.js
@@ -0,0 +1,23 @@
+const router = require('express').Router()
+const controllersRouter = require('../controllers/controller')
+
+router.get('/',controllersRouter.index)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
--- src/server.js
+++ src/server.js
@@ -1,5 +1,15 @@
+const { urlencoded } = require('express')
 const express = require('express')
 const app = express()
+const path = require('path')
+const router = require('./routers/router')
+
+app.use(express(urlencoded,{extends:true}))
+
+app.set('views',path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(router)
+
 
 
 
